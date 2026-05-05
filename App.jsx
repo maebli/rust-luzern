f367f1c3668cf055e4b648d857d2020dd20f0f85
Michael Aebli <doland@duck.com>
2026-05-05 15:48:11 +0200

    rust at toradex

--- App.jsx
+++ App.jsx
@@ -24,7 +24,7 @@ const App = () => {
     program: DEFAULT_PROGRAM,
     talks: [
       { speaker: 'Markus Ineichen', speakerUrl: 'https://github.com/mineichen', title: '— Pilatus: a modular, extensible application framework' },
-      { speaker: 'Leonardo Held', title: '— Rust @ Toradex' },
+      { speaker: 'Leonardo Held', title: '— Understanding the Borrow Checker' },
     ],
   };
 
